b0ed0deb99211129dc203822a555e700a6f812d0
Matthew Jurenka <matt.jurenka@comcast.net>
2023-04-25 06:40:30 +0000

    removing decode after encode

--- fuzz/fuzz_targets/base32ct_unpadded.rs
+++ fuzz/fuzz_targets/base32ct_unpadded.rs
@@ -4,7 +4,5 @@ use base32ct::{Base32Unpadded, Encoding};
 
 fuzz_target!(|value: &[u8]| {
     let mut encode_buf = vec![0; Base32Unpadded::encoded_len(value)];
-    let mut decode_buf = vec![0; Base32Unpadded::encoded_len(value)];
-    let encoded = Base32Unpadded::encode(value, encode_buf.as_mut_slice()).unwrap();
-    let _ = Base32Unpadded::decode(encoded, decode_buf.as_mut_slice()).unwrap();
+    let _ = Base32Unpadded::encode(value, encode_buf.as_mut_slice()).unwrap();
 });
