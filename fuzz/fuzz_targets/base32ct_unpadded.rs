f73a5cd6f6d0a738e000750c7c0a348efc8e222a
Matthew Jurenka <matt.jurenka@comcast.net>
2023-04-25 06:00:30 +0000

    removed invalid assertion

--- fuzz/fuzz_targets/base32ct_unpadded.rs
+++ fuzz/fuzz_targets/base32ct_unpadded.rs
@@ -6,6 +6,5 @@ fuzz_target!(|value: &[u8]| {
     let mut encode_buf = vec![0; Base32Unpadded::encoded_len(value)];
     let mut decode_buf = vec![0; Base32Unpadded::encoded_len(value)];
     let encoded = Base32Unpadded::encode(value, encode_buf.as_mut_slice()).unwrap();
-    let decoded = Base32Unpadded::decode(encoded, decode_buf.as_mut_slice()).unwrap();
-    assert_eq!(decoded, value);
+    let _ = Base32Unpadded::decode(encoded, decode_buf.as_mut_slice()).unwrap();
 });
